707a2c4759ee78fcc09b267e28d434be883bad0e
0l3x <alexhorst58@gmail.com>
2023-03-22 21:50:37 +0100

    Kamera progress
    Kamera eig fertig wollte grad noch Vogelperspektive machen aber mein PC stinkt und macht Auge.
    
    Wir müssen noch die Bälle und den Tisch auf das selbe größenverhältniss bringen

--- BilliardSchool/Assets/Scripts/KameraMovement.cs
+++ BilliardSchool/Assets/Scripts/KameraMovement.cs
@@ -5,26 +5,104 @@ using UnityEngine;
 public class KameraMovement : MonoBehaviour
 {
     [SerializeField] public float distanceFromTarget = 5.0f;
-    [SerializeField] private Transform target;
+    [SerializeField] private Transform whiteBall;   //Braucht den weissen Ball zur Positionierung
+    [SerializeField] private Rigidbody whiteBall_rb; //Den Ball muss man einen Impuls geben können
+    [SerializeField] private Transform poolTable;   //Den Tisch für Vogelperspektive
+    private Vector3 nachVorne;
+    private Vector3 nachVorne_richtung;
     [SerializeField] private float smoothenessTime = 0.2f;
+    [SerializeField] private float forwardOnCue = 20f;
     private float rotationX = 0f;
+    private float rotationY = 0f;
     public float sensivity = 3f;
     private Vector3 smoothVelocity = Vector3.zero;
     private Vector3 currentRotation;
+    private int mode = 0;       //Diese Variable sagt, was genau die Kamera macht. 0 ist z.B. Vogelperspektive, 1 wäre die Kamera dann auf der Kugel fixiert.
+    
+    void CameraAsBirdsEye(){
 
-    //
+        transform.localEulerAngles = new Vector3(90, 0, 90);
+        transform.localPosition = poolTable.localPosition + new Vector3(0,80,0);
+
+    }
+    void CameraOnBall(){
+
+        //Input.mouseScrollDelta.y ------> Wenn der Wert 1: Mausrad nach vorne. Bei -1 nach hinten und 0 ist default
+        switch(Input.mouseScrollDelta.y){
+            case 1:
+                if(distanceFromTarget > 5){
+                    distanceFromTarget--;
+                }
+                break;
+            case -1:
+                if(distanceFromTarget < 25){
+                    distanceFromTarget++;
+                }
+                break;
+            default:
+                break;
+        }
+
+        //--------------------------------------------------------------------
+        // 1. Den Input der Maus Einlesen und die Kamera smooth machen. Der Kamerarichtungsvektor ist also mit dem Vektor "currentRotation" erreichbar
+
+        rotationX += Input.GetAxis("Mouse X") * sensivity;
+        
+        //Diese If sorgt dafür, dass man nicht auf der Y achse überrotiert.
+        if(rotationY <= 75){
+            if(rotationY < -15){
+                rotationY = -15;
+            }
+            else{
+                rotationY -= Input.GetAxis("Mouse Y") * sensivity;      //Hier -= da bei += sonst invertiert ist aus irgend einem Grund?
+            }
+        }
+        else{
+            rotationY = 75;
+        }
+
+        //  Die Kamera smooth machen
+        Vector3 nextRotation = new Vector3 (rotationY, rotationX, 0);
+        currentRotation = Vector3.SmoothDamp(currentRotation, nextRotation, ref smoothVelocity, smoothenessTime);
+
+        //      Siehe "kameraPositioning.png"
+        //      1.
+        Transform transform_copy = transform;
+        transform_copy.localEulerAngles = currentRotation;
+        nachVorne = whiteBall.position + transform_copy.forward * forwardOnCue;
+        //      2.
+        transform.localEulerAngles = currentRotation + new Vector3(15, 0, 0);
+        transform.position = nachVorne - (transform.forward) * distanceFromTarget;
+        
+        if(Input.GetKey("b")){
+
+            whiteBall_rb.velocity = transform.forward * 10;
+
+        }
+
+    }
 
     void Update()
     {
-        rotationX += Input.GetAxis("Mouse X") * sensivity;
+        if(Input.GetKey("a")){
+            if(mode == 0){
+                mode = 1;
+            }
+            else{
+                mode = 0;
+            }
+        }
+        while(!Input.GetKey("a"));
 
-        Debug.Log("RotationX: " + rotationX);
-
-        Vector3 nextRotation = new Vector3 (15, rotationX, 0);
-        currentRotation = Vector3.SmoothDamp(currentRotation, nextRotation, ref smoothVelocity, smoothenessTime);
-
-        transform.localEulerAngles = currentRotation;
-        transform.position = target.position - transform.forward * distanceFromTarget;          //Dieser Command Hält die Kamera an der Kugel fest (Die Kamera wird je nachdem in welche Richtung sie Zeigt um die Kugel herumteleportiert).
-
+        switch (mode){
+            case 0:
+                CameraAsBirdsEye();
+                break;
+            case 1:
+                CameraOnBall();
+                break;
+            default:
+                break;
+        }
     }
 }
