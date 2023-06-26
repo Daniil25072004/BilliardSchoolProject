5cd907e32a1939d21e90ff0153dc6cad5014fec6
TaktiG <dpetukhov25@gmail.com>
2023-06-26 20:58:27 +0200

    snoos

--- BilliardSchool/Assets/Scripts/MainMenu.cs
+++ BilliardSchool/Assets/Scripts/MainMenu.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.Events;
 public class MainMenu : MonoBehaviour
 {
+    private string hitKey = "b";
+    private string cameraKey = "a";
     public void Play() {
 
         SceneManager.LoadScene(1); //Wenn Play gedrückt wird, wird auf die nächste Scene gesprungen
@@ -20,5 +22,11 @@ public class MainMenu : MonoBehaviour
     public void Quit() {
         Debug.Log("Player left the game!");
         Application.Quit();
+    }
+    public void ChangeHitKey() {
+        if(Input.anyKeyDown) {
+        hitKey = Input.inputString;
+        Debug.Log("Key:"+hitKey+" pressed");
+        }
     }
 }
